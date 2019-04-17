176644dbf43c1d590e242c219fcca7379562c4c5
sarmishta <bsarmishta@gmail.com>
2019-04-17 16:10:51 -0700

    first commit

---
+++ ocean_main.cpp
@@ -0,0 +1,86 @@
+
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/* Implement this function in serial_ocean and omp_ocean */
+extern void ocean (int** grid, int xdim, int ydim, int timesteps);
+
+void printGrid(int** grid, int xdim, int ydim){
+int i, j;
+  for(i=0;i<ydim;i++){
+    for(j=0;j<xdim;j++)
+      printf("%d  ",grid[i][j]);
+    printf("\n");
+  }
+
+}
+
+int main(int argc, char* argv[])
+{
+    int xdim,ydim,timesteps;
+    int** grid;
+    int i,j,t;
+
+    
+    /********************Get the arguments correctly (start) **************************/
+    /* 
+    Three input Arguments to the program
+    1. X Dimension of the grid
+    2. Y dimension of the grid
+    3. number of timesteps the algorithm is to be performed
+    */
+    
+    if (argc!=4) {
+        cout << "The Arguments you entered are wrong." << endl;
+        cout << "./serial_ocean <x-dim> <y-dim> <timesteps>" << endl;
+        return EXIT_FAILURE;
+    } else {
+        xdim = atoi(argv[1]);
+        ydim = atoi(argv[2]);
+        timesteps = atoi(argv[3]);
+    }
+    ///////////////////////Get the arguments correctly (end) //////////////////////////
+
+
+    /*********************create the grid as required (start) ************************/
+    /*
+    The grid needs to be allocated as per the input arguments and randomly initialized.
+    Remember during allocation that we want to guarantee a contiguous block, hence the
+    nasty pointer math.
+
+    To test your code for correctness please comment this section of random initialization.
+    */
+    grid = (int**) malloc(ydim*sizeof(int*));
+    int *temp = (int*) malloc(xdim*ydim*sizeof(int));
+    for (i=0; i<ydim; i++) {
+        grid[i] = &temp[i*xdim];
+    }
+    for (i=0; i<ydim; i++) {
+        for (j=0; j<xdim; j++) {
+            grid[i][j] = rand()%100000;
+        }
+    }
+    ///////////////////////create the grid as required (end) //////////////////////////
+
+
+	chrono::milliseconds total;
+
+    auto start = chrono::high_resolution_clock::now(); // Start the time measurment here before the algorithm starts
+
+    ocean(grid, xdim, ydim, timesteps);
+    
+    total = chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start);  // End the time measuremnt here since the algorithm ended
+    cout << "Total Execution time: "  << total.count() << "ms" << endl; // print the execution time in ms
+    printGrid(grid,xdim,ydim);
+    
+    
+
+    // Free the memory we allocated for grid
+   free(temp);
+   free(grid);
+
+    return EXIT_SUCCESS;
+}
---
+++ serial_ocean.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void ocean(int **grid, int xdim, int ydim, int timesteps)
+{
+	/********************* the red-black algortihm (start)************************/
+	/*
+    In odd timesteps, calculate indeces with - and in even timesteps, calculate indeces with * 
+    See the example of 6x6 matrix, A represents the corner elements. 
+        A A A A A A
+        A - * - * A
+        A * - * - A
+        A - * - * A
+        A * - * - A
+        A A A A A A 
+    */
+
+	// PUT YOUR CODE HERE
+     int steps,i,j,gridLocation;
+
+    for(steps=0; steps<timesteps;steps++){
+        for(i=1; i<xdim-1; i++){
+            gridLocation = (i+steps)%2;// for updating even locations at even timesteps and odd at odd timesteps
+            for(j=1+gridLocation; j<ydim-1; j=j+2) {
+	            grid[i][j] = (grid[i][j] + grid[i-1][j] + grid[i+1][j]+ grid[i][j-1] + grid[i][j+1])/5;//avg 
+         }
+        }
+    }
+	
+	/////////////////////// the red-black algortihm (end) ///////////////////////////
+
+
+
+
+}
